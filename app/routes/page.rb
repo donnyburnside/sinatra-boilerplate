c64c3f206fa6c5bbdfb24784dfffbd0a50457477
Donny Burnside <donnyburnside1@gmail.com>
2016-04-30 21:48:43 +0100

    Removed files no longer used in the project

--- app/routes/page.rb
+++
@@ -1,3 +0,0 @@
-get "/page" do
-  haml :"page", :layout => :layout
-end
--- tests/test_helper.rb
+++
@@ -1,7 +0,0 @@
-ENV['RACK_ENV'] = 'test'
-require 'minitest/autorun'
-require 'rack/test'
-require 'tilt/erb'
-require 'mocha/mini_test'
-
-require File.expand_path '../../app.rb', __FILE__
