449ef3301f730aea8ce8bff9e4eded3270fc9338
Donny Burnside <donnyburnside1@gmail.com>
2016-05-01 12:42:30 +0100

    Remove saving mechanism

--- app/controllers/post/new.rb
+++ app/controllers/post/new.rb
@@ -1,12 +1,3 @@
 get "/post/new" do
   haml :"post/new", :layout => :layout
 end
-
-post "/post/new" do
-  @post = Post.new(params[:post])
-  if @post.save
-    redirect "/post/#{@post.id}"
-  else
-    haml :"post/index", :layout => :layout
-  end
-end
