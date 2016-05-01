8a571f60f17b43e5765aa9a3cdb9c0dd9ced20cd
Donny Burnside <donnyburnside1@gmail.com>
2016-05-01 12:44:17 +0100

    Re-added saving mechanism and changed view

--- app/controllers/post/new.rb
+++ app/controllers/post/new.rb
@@ -1,3 +1,12 @@
 get "/post/new" do
-  haml :"post/new", :layout => :layout
+  haml :"post/index", :layout => :layout
+end
+
+post "/post/new" do
+  @post = Post.new(params[:post])
+  if @post.save
+    redirect "/post/#{@post.id}"
+  else
+    haml :"post/index", :layout => :layout
+  end
 end
