9d5a7b0ad13244bf4efcc581f3f89059625a2a6f
Donny Burnside <donnyburnside1@gmail.com>
2016-05-08 15:02:22 +0100

    Included auth.rb setup file

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require "./setup/libs"    # Require libraries
 require "./setup/config"  # Require config
+require "./setup/auth"    # Require auth
 require "./setup/db"      # Require db setup
 
 Dir["./app/**/*.rb"].each { |file| require file }
